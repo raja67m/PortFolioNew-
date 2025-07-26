7d2614559b93b1edc8ae506b0415b18c5f703469
raja <rajalingam@210901@gmail.com>
2025-07-26 19:42:17 +0530

    home page updated

--- protfolio/src/home.js
+++ protfolio/src/home.js
@@ -1,5 +1,6 @@
 import './home.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import profile from './raja.png';
 
 
 function Home(){
@@ -9,16 +10,21 @@ return(
        
       <div class="row">
                  {/* profile intro start */}
-         <div class="column" id="intro">
-          <h1>Hello welcome,<br></br>I'm Rajalingam</h1>
-           <p>Lorem ipsum" is placeholder text commonly used in design and development <br></br>to fill space and demonstrate layout without distracting from the visual presentation.<br></br> While "lorem" itself is not a CSS property or value, it is frequently used within<br></br> HTMLtags to generate this dummy text.</p> 
-            <button type="button" class="btn btn-primary">Contact</button>         
-         </div>
+            <div class="col-lg-8" id="intro">
+                <h1>Hello welcome,<br></br>I'm Rajalingam</h1>
+                 <p>Lorem ipsum" is placeholder text commonly used in design and development to fill space and demonstrate layout without distracting from the visual presentation. While "lorem" itself is not a CSS property or value, it is frequently used within HTMLtags to generate this dummy text.</p> 
+               {/* contact button start */}
+                <div class="row-lg-12">
+                 <button type="button" class="btn btn-primary" id='contact'>Contact</button>         
+                 <button type="button" class="btn btn-primary" id='github'>Git Hub</button>         
+                 </div> 
+                 {/* contact button end */}
+            </div>
                  {/* profile intro end */}
 
                  {/* profile image  start*/}
-                 <div>
-                  <img src="" alt='Not Visible'/>
+                 <div class="col-lg-4 col-sm-12" id='profile'>
+                  <img src={profile} class="img-thumbnail" alt='Not Visible'/>
                  </div>
                      {/* profile image  end*/}
 
