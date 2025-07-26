da24d6feacd76c02c4c2188c995f66ab7b3a4d60
raja <rajalingam@210901@gmail.com>
2025-07-26 18:37:24 +0530

    work with home page

--- protfolio/src/home.js
+++ protfolio/src/home.js
@@ -4,9 +4,27 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function Home(){
 
-return(<div className="Home">
+return(
+   <div class="container">
+       
+      <div class="row">
+                 {/* profile intro start */}
+         <div class="column" id="intro">
+          <h1>Hello welcome,<br></br>I'm Rajalingam</h1>
+           <p>Lorem ipsum" is placeholder text commonly used in design and development <br></br>to fill space and demonstrate layout without distracting from the visual presentation.<br></br> While "lorem" itself is not a CSS property or value, it is frequently used within<br></br> HTMLtags to generate this dummy text.</p> 
+            <button type="button" class="btn btn-primary">Contact</button>         
+         </div>
+                 {/* profile intro end */}
 
-<h1>Hello welcome</h1>
+                 {/* profile image  start*/}
+                 <div>
+                  <img src="" alt='Not Visible'/>
+                 </div>
+                     {/* profile image  end*/}
+
+      </div>
+
+
 
    </div>
 );
