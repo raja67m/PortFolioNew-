0386a7734b9153c17609445e75af8239915df787
raja <rajalingam@210901@gmail.com>
2025-07-30 19:31:59 +0530

    work with about page

--- protfolio/src/about.js
+++ protfolio/src/about.js
@@ -1,6 +1,56 @@
+import './home.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import profile from './raja.jpg';
+import { BsTwitterX } from "react-icons/bs";
+import { SiGithub } from "react-icons/si";  
+import { FaLinkedin } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
 function About(){
-    return<>
-    <h1>About</h1>
-    </>
+    return<div class="container-fluid">
+
+                <div class="col">
+                    <div class="row">
+                        {/* profile image with links  start*/}
+                            <div class="col-lg-5">
+                               <img src={profile}  alt='Not Visible'/> 
+                                {/* social links start */}
+                                    <div id="social-links">
+                                         <ul>
+                                            <li><a href="#"><BsTwitterX/></a></li>
+                                             <li><a href="#" target="_blank"><SiGithub/></a></li>
+                                             <li><a href="#"><FaLinkedin/></a></li>
+                                             <li><a href="#"><FaInstagram/></a></li>
+                                          </ul>
+                                          
+                                    </div>
+                                {/* social links end */}
+
+
+                            </div>
+
+                        {/* profile image with links  end*/}
+
+                        {/* About our exp start & resume start */}
+                         <div class="col-lg-7">
+                              <h1>Hello welcome,<br></br>I'm Rajalingam</h1>
+                 <p>Lorem ipsum" is placeholder text commonly used in design and development to fill space and demonstrate layout without distracting from the visual presentation. While "lorem" itself is not a CSS property or value, it is frequently used within HTMLtags to generate this dummy text.</p> 
+               {/* contact button start */}
+                <div class="row-lg-12">
+                 <button type="button" class="btn btn-primary" id='contact'>Contact</button>         
+                 <button type="button" class="btn btn-primary" id='github'>Git Hub</button>         
+                 </div> 
+                 {/* contact button end */}
+
+                            </div>
+                        
+                        {/* About our exp start & resume end */}
+
+
+                    </div>
+
+                </div>
+</div>
+    
 }
 export default About;
