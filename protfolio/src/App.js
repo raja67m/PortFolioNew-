914530e4e9f0c23894649709d62e9d87ccb14714
raja <rajalingam@210901@gmail.com>
2025-08-04 18:56:43 +0530

    about page working

--- protfolio/src/App.js
+++ protfolio/src/App.js
@@ -4,6 +4,7 @@ import Project from "./project";
 import Contact from "./contact";
 import NavigationBar from "./navbar";
 
+
 import { Routes,Route} from "react-router";
 
 
@@ -13,14 +14,14 @@ function App() {
   return (
    <div>
 <NavigationBar/>
+ 
     <Routes>
-     
-      <Route path="/" element={<Home/>}/>
+     <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/project" element={<Project/>}/>
       <Route path="/contact" element={<Contact/>}/>
     </Routes>
-   
+  
    </div>
   );
 }
--- protfolio/src/about.js
+++ protfolio/src/about.js
@@ -7,22 +7,21 @@ import { FaLinkedin } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 
 function About(){
-    return<div class="container-fluid">
+    return<div class="container-fluid" id='about'>
 
-                <div class="col">
+                <div class="col" >
                     <div class="row">
                         {/* profile image with links  start*/}
-                            <div class="col-lg-5">
-                               <img src={profile}  alt='Not Visible'/> 
+                            <div class="col-lg-4" >
+                               <img src={profile}   alt='Not Visible'/> 
                                 {/* social links start */}
-                                    <div id="social-links">
+                                    <div class="row-lg-5" id="social-links">
                                          <ul>
                                             <li><a href="#"><BsTwitterX/></a></li>
-                                             <li><a href="#" target="_blank"><SiGithub/></a></li>
+                                             <li><a href="#"><SiGithub/></a></li>
                                              <li><a href="#"><FaLinkedin/></a></li>
                                              <li><a href="#"><FaInstagram/></a></li>
                                           </ul>
-                                          
                                     </div>
                                 {/* social links end */}
 
@@ -32,15 +31,15 @@ function About(){
                         {/* profile image with links  end*/}
 
                         {/* About our exp start & resume start */}
-                         <div class="col-lg-7">
+                         <div class="col-lg-8" id="summary">
                               <h1>Hello welcome,<br></br>I'm Rajalingam</h1>
-                 <p>Lorem ipsum" is placeholder text commonly used in design and development to fill space and demonstrate layout without distracting from the visual presentation. While "lorem" itself is not a CSS property or value, it is frequently used within HTMLtags to generate this dummy text.</p> 
-               {/* contact button start */}
-                <div class="row-lg-12">
-                 <button type="button" class="btn btn-primary" id='contact'>Contact</button>         
-                 <button type="button" class="btn btn-primary" id='github'>Git Hub</button>         
-                 </div> 
-                 {/* contact button end */}
+                                 <p>Lorem ipsum" is placeholder text commonly used in design and development to fill space and demonstrate layout without distracting from the visual presentation. While "lorem" itself is not a CSS property or value, it is frequently used within HTMLtags to generate this dummy text.</p> 
+                                     {/* contact button start */}
+                                         <div class="row-lg-12">
+                                          <button type="button" class="btn btn-primary" id='contact'>My Project</button>         
+                                             <button type="button" class="btn btn-primary" id='github'>Download CV</button>         
+                                   </div> 
+                                        {/* contact button end */}
 
                             </div>
                         
