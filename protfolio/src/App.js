a7f476fa52a0951f08942d06aa023fd7b3f6578c
raja <rajalingam@210901@gmail.com>
2025-08-15 19:59:57 +0530

    style type style

--- protfolio/src/App.js
+++ protfolio/src/App.js
@@ -17,6 +17,7 @@ function App() {
  
     <Routes>
      <Route path="/" element={<Home/>}/>
+     
       <Route path="/about" element={<About/>}/>
       <Route path="/project" element={<Project/>}/>
       <Route path="/contact" element={<Contact/>}/>
--- protfolio/src/tabs.js
+++ protfolio/src/tabs.js
@@ -24,7 +24,27 @@ const handleClick=(event)=>{
          {/* tab content is start */}
         <div class='col'>
             <div class={`row ${active === 'tab-1' ? 'active' : ''}`} id='tab-page'>
-                <p>This is tab 1</p>
+              <div class="col-lg-6" id='skill'>
+                   <h6>Web developmemnt</h6>
+                 <ul>
+                  <li>Html</li>
+                  <li>Css</li>
+                  <li>Javascript</li>
+                  <li>React.Js</li>
+                  <li>React Redux</li>
+                  <li>Bootstrap</li>
+                 </ul>
+                   <h6>Productive Tools</h6>
+                  <ul>
+                  <li>Vs code</li>
+                  <li>Micorsoft Excel</li>
+                  <li>Micorsoft Word</li>
+                 </ul>
+                   <h6>Addtional Skills</h6>
+                  <ul>
+                   <li>Typing(High Accuracy & Speed)</li>
+                  </ul>
+                </div>
 
             </div>
              <div class={`row ${active === 'tab-2' ? 'active' : ''}`} id='tab-page'>
